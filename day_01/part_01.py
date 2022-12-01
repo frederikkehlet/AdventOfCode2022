fc318d8773dd1a628331e3d808bae32697a28069
Frederik Hundahl Kehlet <fke@netcompany.com>
2022-12-01 08:30:59 +0100

    day 1

---
+++ day_01/part_01.py
@@ -0,0 +1,15 @@
+with open('input.txt', 'r') as file:
+    lines = [line.strip() for line in file.readlines()]
+    indices = [ix for ix, line in enumerate(lines) if line == '']
+    indices.insert(0,0)
+    elves = [lines[i:j] for i,j in zip(indices, indices[1:]+[None])]
+
+    for elf in elves:        
+        cals = [int(cal) for cal in elf if cal != '']
+        elves[elves.index(elf)] = sum(cals)
+
+    print(max(elves))
+        
+
+    
+    
