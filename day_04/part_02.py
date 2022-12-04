91a1022483a77ab63a6faeac2f2dc4fe2731597f
Frederik <frederik.kehlet@outlook.dk>
2022-12-04 09:35:45 +0100

    day 4

---
+++ day_04/part_02.py
@@ -0,0 +1,13 @@
+with open('input.txt', 'r') as file:
+    pairs = [line.strip().split(',') for line in file.readlines()]   
+    fully_contained = 0
+
+    for pair in pairs:
+        elf1 = [int(num) for num in pair[0].split('-')]
+        elf2 = [int(num) for num in pair[1].split('-')]
+
+        if ((elf1[0] >= elf2[0] and elf1[0] <= elf2[1]) or (elf1[1] >= elf2[0] and elf1[1] <= elf2[1])
+        or (elf2[0] >= elf1[0] and elf2[0] <= elf1[1]) or (elf2[1]) >= elf1[0] and elf2[1] <= elf1[1]):
+            fully_contained = fully_contained + 1
+
+    print(fully_contained)
