dbc47f8d8714c8e77e796de4152bcae70c8eb32f
Frederik Kehlet <frederik.kehlet@outlook.dk>
2022-12-04 10:49:41 +0100

    renamed variable

--- day_04/part_02.py
+++ day_04/part_02.py
@@ -1,6 +1,6 @@
 with open('input.txt', 'r') as file:
     pairs = [line.strip().split(',') for line in file.readlines()]   
-    fully_contained = 0
+    overlaps = 0
 
     for pair in pairs:
         elf1 = [int(num) for num in pair[0].split('-')]
@@ -8,6 +8,6 @@ with open('input.txt', 'r') as file:
 
         if ((elf1[0] >= elf2[0] and elf1[0] <= elf2[1]) or (elf1[1] >= elf2[0] and elf1[1] <= elf2[1])
         or (elf2[0] >= elf1[0] and elf2[0] <= elf1[1]) or (elf2[1]) >= elf1[0] and elf2[1] <= elf1[1]):
-            fully_contained = fully_contained + 1
+            overlaps = overlaps + 1
 
-    print(fully_contained)
+    print(overlaps)
